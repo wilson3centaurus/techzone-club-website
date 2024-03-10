9dd7148a95c0f7e57e03b13df306133fb8119ffc
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-03-10 13:12:05 +0200

    update

--- script.js
+++ script.js
@@ -6,20 +6,6 @@ const slideshowImages = [
     'images/background/background4.jpg',
     'images/background/background5.jpg',
     'images/background/background6.jpg',
-    'images/background/background7.jpg',
-    'images/background/background8.jpg',
-    'images/background/background9.jpg',
-    'images/background/background10.jpg',
-    'images/background/background11.jpg',
-    'images/background/background12.jpg',
-    'images/background/background13.jpg',
-    'images/background/background14.jpg',
-    'images/background/background15.jpg',
-    'images/background/background16.jpg',
-    'images/background/background17.jpg',
-    'images/background/background18.jpg',
-    'images/background/background19.jpg',
-    'images/background/background20.jpg',
 ];
 
 let currentImageIndex = 0;
