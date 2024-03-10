a14b9c33adf3cf4b63c3c96bb6a1e707c5fb5e28
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-03-10 11:25:52 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
 const slideshowImages = [
-    'images/image1.jpg',
-    'images/image2.jpg',
-    'images/image3.jpg',
+    'images/background/background1.jpg',
+    'images/background/background2.jpg',
+    'images/background/background3.jpg',
+    'images/background/background4.jpg',
+    'images/background/background5.jpg',
+    'images/background/background6.jpg',
+    'images/background/background7.jpg',
+    'images/background/background8.jpg',
+    'images/background/background9.jpg',
+    'images/background/background10.jpg',
+    'images/background/background11.jpg',
+    'images/background/background12.jpg',
+    'images/background/background13.jpg',
+    'images/background/background14.jpg',
+    'images/background/background15.jpg',
+    'images/background/background16.jpg',
+    'images/background/background17.jpg',
+    'images/background/background18.jpg',
+    'images/background/background19.jpg',
+    'images/background/background20.jpg',
 ];
 
 let currentImageIndex = 0;
@@ -12,5 +29,15 @@ function changeBackground() {
     currentImageIndex = (currentImageIndex + 1) % slideshowImages.length;
 }
 
-setInterval(changeBackground, 5000); //Change the image every 5 seconds (adjust as needed)
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + slideshowImages.length) % slideshowImages.length;
+    changeBackground();
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % slideshowImages.length;
+    changeBackground();
+}
+
+setInterval(changeBackground, 3000);
 
